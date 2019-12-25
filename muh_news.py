affb0c47a85cfc11bac72bcc2b8f0b5758271f5c
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-25 02:01:57 -0600

    Update muh_news.py

--- muh_news.py
+++ muh_news.py
@@ -11,7 +11,6 @@ from newspaper import Article
 from newspaper.article import ArticleException
 
 airtab = Airtable(os.environ['articles_db'], 'links', os.environ['AIRTABLE_API_KEY'])
-
 airtab_log = Airtable(os.environ['log_db'], 'log', os.environ['AIRTABLE_API_KEY'])
 
 
@@ -25,6 +24,7 @@ def wrap_it_up(t0, new, total=None, function=None):
 
 
 def scrape_pages():
+    t0, i = time.time(), 0
     records = airtab.get_all(view='needs news_scraper.py')
     for record in records:
         this_dict = {}
@@ -39,18 +39,18 @@ def scrape_pages():
             article.nlp()
             this_dict['kwds2'] = ', '.join(article.keywords)
             this_dict['excerpt2'] = article.summary
+            i += 1
         except ArticleException as err:
             print(f"🤬: {err}")
             this_dict['oops'] = 'newspaper fucked up'
         finally:
             airtab.update(record['id'], this_dict)
-    # wrap_it_up('scrape_pages', t0, len(records), total=None)
-    return len(records)
+    wrap_it_up(t0=t0, new=i, total=len(records), function='scrape_pages')
 
 
 def extract_kwds():
-    records = airtab.get_all(view='needs kwds3', fields=[
-        'title', 'body2', 'clean title'])
+    t0 = time.time()
+    records = airtab.get_all(view='needs kwds3', fields=['title', 'body2', 'clean title'])
     for record in records:
         this_dict = {}
         if 'clean title' in record['fields']:
@@ -58,41 +58,40 @@ def extract_kwds():
                 '\n' + record['fields']['body2']
         else:
             data = record['fields']['body2']
-        this_dict['kwds3'] = ', '.join(
-            gen_kwds(data, words=15, split=True, lemmatize=True))
+        this_dict['kwds3'] = ', '.join(gen_kwds(data, words=15, split=True, lemmatize=True))
         airtab.update(record['id'], this_dict)
+    wrap_it_up(t0=t0, new=len(records), total=len(records), function='extract_kwds')
 
 
 def clean_urls():
+    t0 = time.time()
     records = airtab.get_all(view='needs parsing', fields='clean url')
     for record in records:
         x = urllib.parse.urlparse(record['fields']['clean url'])
         this_dict = {}
-        this_dict['parsed_at'] = datetime.datetime.utcnow().replace(
-            tzinfo=datetime.timezone.utc).strftime('%Y-%m-%d %H:%M')
+        this_dict['parsed_at'] = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).strftime('%Y-%m-%d %H:%M')
         this_dict['scheme'] = x.scheme
         this_dict['params'] = x.params
         this_dict['query'] = x.query
         this_dict['netloc'] = x.netloc
         this_dict['path'] = x.path
         airtab.update(record['id'], this_dict)
+    wrap_it_up(t0=t0, new=len(records), total=len(records), function='clean_urls')
 
 
 def upload_img():
+    t0 = time.time()
     records = airtab.get_all(view='needs image', fields='img_url')
     for record in records:
         this_dict = {'img': [{'url': record['fields']['img_url']}]}
         airtab.update(record['id'], this_dict)
+    wrap_it_up(t0=t0, new=len(records), total=len(records), function='upload_img')
 
 
 def main():
-    t0 = time.time()
-    new = scrape_pages()
     extract_kwds()
     clean_urls()
     upload_img()
-    funcs = ['scrape_pages', 'extract_kwds', 'clean_urls', 'upload_img']
-    wrap_it_up(t0, new, function=funcs)
 
 
 if __name__ == "__main__":
