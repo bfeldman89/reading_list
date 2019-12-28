2f8f667ebbc3cb8100d668e45d2a0a2194b88913
bfeldman89 <26087892+bfeldman89@users.noreply.github.com>
2019-12-27 18:27:28 -0600

    Update common.py
    this will prevent me from having to manually copy and past from single select field into the linked record field. Doing a switch/case method in the common module makes the main module from being unreadable.

--- common.py
+++ common.py
@@ -24,11 +24,19 @@ cloudinary.config(cloud_name='bfeldman89',
 muh_headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
 
 
+my_funcs = {'get_reading_list': 'rec43zeXFYciU2ZUH',
+            'clean_urls': 'rec6YZFb0DMukAXLv',
+            'extract_kwds': 'recdrZbvp7pWvI4qK',
+            'scrape_pages': 'recJJ2UyT9QWCPLh4',
+            'upload_img': 'recjZTLZAMT1qrihh'}
+
+
 def wrap_from_module(module):
     def wrap_it_up(t0, new=None, total=None, function=None):
         this_dict = {
             'module': module,
             'function': function,
+            '_function': my_funcs[function],
             'duration': round(time.time() - t0, 2),
             'total': total,
             'new': new
